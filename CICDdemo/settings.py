3f63fa3567813812d6a37ac252c6ffe883a2dacd
Dipesh Basnet <dipeshbasnet@Dipeshs-MacBook-Pro.local>
2024-01-25 19:10:27 -0600

    [Added] STATIC_DIRS

--- CICDdemo/settings.py
+++ CICDdemo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
